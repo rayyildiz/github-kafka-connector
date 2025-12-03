59dd10e75709ee3464f8152796449118d92faf43
Ramazan A. <r@ayyildiz.ai>
2025-12-03 18:59:21 +0300

    Refactor `GithubApiSpec` to use `AnyWordSpec` and correct ScalaTest matchers.

--- src/test/scala/com/rayyildiz/connect/GithubApiSpec.scala
+++ src/test/scala/com/rayyildiz/connect/GithubApiSpec.scala
@@ -1,16 +1,16 @@
 package com.rayyildiz.connect
 
-import org.scalatest.matchers.must.Matchers
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class GithubApiSpec extends Matchers {
+class GithubApiSpec extends AnyWordSpec with Matchers {
 
-  "GithubApi" should "call repository" in {
-    val body= GithubApi.call("https://api.github.com/repos/kubernetes/kubernetes/issues")
+  "GithubApi" should {
+    "call repository" in {
+      val body = GithubApi.call("https://api.github.com/repos/kubernetes/kubernetes/issues")
 
-    body.isLeft shouldBe(false)
-    body.isRight shouldBe(true)
-
+      body.isLeft shouldBe false
+      body.isRight shouldBe true
+    }
   }
-
 }
